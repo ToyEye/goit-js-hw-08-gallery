94972ef4aa865deb2bbae67848aae2ad30d65fc1
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-09-21 17:09:57 +0300

    2

--- js/gallery.js
+++ js/gallery.js
@@ -1,46 +1,50 @@
+// Разбей задание на несколько подзадач:
+
+// Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
 import galleryItems from '../app.js';
 
-{/* <li class="gallery__item">
+const galleryContainerRef = document.querySelector('.js-gallery');
+const cartMarkUp = createWeb(galleryItems);
+
+function createWeb(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+     <li class="gallery__item">
   <a
     class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+    href="${original}"
   >
     <img
       class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</li> */}
+</li>`}
+  ).join('')
+};
+galleryContainerRef.insertAdjacentHTML('beforeend',cartMarkUp);
 
+galleryContainerRef.addEventListener('click',openModalWindow);
 
-const galleryContainerRef = document.querySelector('.js-gallery');
-
-const listRef = document.createElement('li');
-listRef.classList.add('gallery__item');
-
-const linkRef = document.createElement('a');
-linkRef.classList.add('gallery__link');
-linkRef.setAttribute('href', 'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg');
-
-const imageRef = document.createElement('img');
-imageRef.classList.add('gallery__image');
-imageRef.setAttribute('src', 'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg');
-imageRef.setAttribute('data-source', 'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg');
-imageRef.setAttribute('alt', 'Tulips');
-
-linkRef.appendChild(imageRef);
-listRef.appendChild(linkRef);
-galleryContainerRef.appendChild(listRef);
+const modalWindow = document.querySelector('div.lightbox');
 
 
 
 
-// const openModalWindowRef = document.querySelector('js-lightbox');
 
-// openModalWindowRef.addEventListener('click',openMidal)
+function openModalWindow() {
+  if (modalWindow.contains('is-open')) {
+    modalWindow.classList.add('is-open')
+  } else {
+    modalWindow.classList.remove('is-open');
+  }
 
-// function openMidal() {
-//     if()
-// }
+}
